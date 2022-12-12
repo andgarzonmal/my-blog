b91ae575098cf8cdd3abd3e3eeb9ae8533077253
andgarzonmal <andgarzonmal@unal.edu.co>
2022-12-12 13:59:17 -0500

    Add regex validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
                        uniqueness: { case_sensitive: false  }, 
                        length: { minimum: 3, maximum:25 }
   
-  VALID_EMAIL_FORMAT =
+  VALID_EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
                     uniqueness: { case_sensitive: false  }, 
                     length: { maximum:125 }
