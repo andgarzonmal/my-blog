5765979b7456bb8f609023320f275968a8905bfb
andgarzonmal <andgarzonmal@unal.edu.co>
2022-12-12 13:53:57 -0500

    Add validations to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  validates :username, presence: true, 
+                       uniqueness: { case_sensitive: false  }, 
+                       length: { minimum: 3, maximum:25 }
   
+  VALID_EMAIL_FORMAT =
+  validates :email, presence: true, 
+                    uniqueness: { case_sensitive: false  }, 
+                    length: { maximum:125 }
+                    format: { width: VALID_EMAIL_FORMAT } 
 end
